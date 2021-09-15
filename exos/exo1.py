01e25f6a212ecc93854cf2b0f64beff03da40bc5
cdufour <opusidea@gmail.com>
2021-09-15 13:49:33 +0000

    j1 pm

---
+++ exos/exo1.py
@@ -0,0 +1,14 @@
+'''
+*** Exo 1 ***
+Demander à l'utilisateur de saisir un chiffre
+Si le chiffre saisi est supérieur à guess_number
+on affiche "c'est moins"
+Si le chiffre saisi est inférieur à guess_number
+on affiche "c'est plus"
+Si le chiffre saisi est égal à guess_number
+on affiche "Bravo, tu as deviné !"
+'''
+print("*** EXO 1: chiffre mystère à deviner ***")
+guess_number = 42
+
+# votre code ici
