6c354d416d1886d4cde284329b40d0435faea39c
cdufour <opusidea@gmail.com>
2021-10-05 14:42:57 +0000

    j12 end

--- python/test_calc.py
+++ python/test_calc.py
@@ -4,8 +4,8 @@ from calc import double, triple
 class TestCalcModule(unittest.TestCase):
   def test_double(self):
     self.assertEqual(double(6), 12)
-    self.assertEqual(double(6), 13)
+    #self.assertEqual(double(6), 13)
 
   def test_triple(self):
     self.assertEqual(triple(6), 18)
-    self.assertEqual(triple(6), 19)
+    #self.assertEqual(triple(6), 19)
---
+++ python/test_circle.py
@@ -0,0 +1,18 @@
+import unittest
+from math import pi
+from circle import circleArea
+
+class TestCircleModule(unittest.TestCase):
+  def test_circleArea(self):
+    self.assertAlmostEqual(circleArea(1), pi)
+    self.assertAlmostEqual(circleArea(0), 0)
+    self.assertAlmostEqual(circleArea(2.1), pi * 2.1**2)
+
+  def test_values(self):
+    self.assertRaises(ValueError, circleArea, -2)
+
+  def test_types(self):
+    self.assertRaises(TypeError, circleArea, True)
+    self.assertRaises(TypeError, circleArea, "coucou")
+
+
